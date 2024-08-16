b6345f84db2d979ae460a5dbfc3d8f995cbd8ab8
Eden Iyanda <edeniyanda@gmail.com>
2024-08-16 21:48:19 +0100

    Update: Wrote Decrypt Ciphers

--- Cesear Cipher/main.py
+++ Cesear Cipher/main.py
@@ -1,33 +1,56 @@
 alphabets = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
 
-word = "Eden is a bo"
+word = "The Romans are coming tomorrow to destroy you"
 code = 4
 
-encrypted = ""
 
-for char in word:
-    if char != " ":
-        indexofword = alphabets.index(char.upper())
-        if indexofword + code >= len(alphabets):
-            overflow = (indexofword+code)- len(alphabets)
-            print(overflow)
-            newindex = overflow
-            print(newindex)
+def encryptword(word, code):
+    encrypted = ""
+
+    for char in word:
+        if char != " ":
+            indexofword = alphabets.index(char.upper())
+            if indexofword + code >= len(alphabets):
+                newindex = (indexofword+code)- len(alphabets)
+            else:
+                newindex = indexofword+code
+            encryptedchar = alphabets[newindex]
+            if char.islower():
+                encryptedchar = encryptedchar.lower()
+            else:
+                encryptedchar = encryptedchar.upper()
+            encrypted = encrypted + encryptedchar
         else:
-            newindex = indexofword+code
-        encryptedchar = alphabets[newindex]
-        if char.islower():
-            encryptedchar = encryptedchar.lower()
+            encrypted = encrypted + char
+
+    return encrypted
+    
+print("Encrypted word", encryptword(word, 4))
+
+decrypt = encryptword(word, 4)
+print(decrypt)
+
+def decryptword(word, code):
+    decrypted = ""
+
+    for char in decryptword:
+        if char != " ":
+            indexofword = alphabets.index(char.upper())
+            newindex = indexofword - code
+            decryptedchar = alphabets[newindex]
+            if char.islower():
+                decryptedchar = decryptedchar.lower()
+            else:
+                decryptedchar = decryptedchar.upper()
+            decrypted = decrypted + decryptedchar
         else:
-            encryptedchar = encryptedchar.upper()
-        encrypted = encrypted + encryptedchar
-    else:
-        encrypted = encrypted + char
-print(encrypted)
+            decrypted = decrypted + char
+
+    return decrypted
 
 
-
+# decryptword(decrypt, 4)
 
 
 
